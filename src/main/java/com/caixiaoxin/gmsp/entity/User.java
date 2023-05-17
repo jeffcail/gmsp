90432de459900ec44227955c9e37c41a78b6e621
jeffcail <cljcailianjie@163.com>
2023-05-17 22:17:01 +0800

    springboot集成mybatis实现简单的数据查询

---
+++ src/main/java/com/caixiaoxin/gmsp/entity/User.java
@@ -0,0 +1,16 @@
+package com.caixiaoxin.gmsp.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Integer id;
+    private String username;
+    private String password;
+    private String nickname;
+    private String email;
+    private String phone;
+    private String address;
+
+}
